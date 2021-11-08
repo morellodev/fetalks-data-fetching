ab39daa76906668ace3431714352e684979e058d
Dennis Morello <dennis.morello@contentwise.com>
2021-11-08 20:14:36 +0100

    feat(api): user ids are now uuids

--- packages/server/index.js
+++ packages/server/index.js
@@ -2,8 +2,8 @@ const faker = require("faker");
 
 module.exports = () => {
   return {
-    users: Array.from({ length: 12 }, (_, index) => ({
-      id: index,
+    users: Array.from({ length: 12 }, () => ({
+      id: faker.datatype.uuid(),
       name: faker.name.findName(),
       jobTitle: faker.name.jobTitle(),
       email: faker.internet.email(),
